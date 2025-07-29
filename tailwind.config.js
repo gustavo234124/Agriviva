38249f49387e9f08981969bed146c2eb4db16994
Gustavo <gustavoaguilar@192.168.1.251>
2025-07-29 14:08:28 -0600

    integracion de estructura principal e instalacion de dependencias tailwind a demas de creacion de navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'text-primary': 'var(--color--text-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+      },
+    },
+  },
+  plugins: [],
+};
