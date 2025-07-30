e56ffdbc3f634a34a8739b9219ededf6734d85c2
Gustavo <gustavoaguilar@192.168.1.251>
2025-07-30 11:19:14 -0600

    integracion archivos pdf por producto y descarga dinamica

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 
 export default function ProductCards() {
-  // Estado para saber cuál modal está abierta (null = ninguna)
+  // MAnejador de estado para el modal (abierto o cerrado = null)
   const [openModal, setOpenModal] = useState(null);
 
-  // Datos de ejemplo para cada card
+  // Datos para card
 const cards = [
-  { id: 1, title: "Alfa-Desarrollo", text: "Alfa-Desarrollo", price: "$50", modalContent: "...", headerColor: "#5eb853" },
-  { id: 2, title: "Brote-Fuerte", text: "Brote-Fuerte", price: "$50", modalContent: "...", headerColor: "#8c5b30" },
-  { id: 3, title: "Verde-Forte", text: "Verde-Forte", price: "$50", modalContent: "...", headerColor: "#74c272" },
-  { id: 4, title: "Optimus-Vital", text: "Optimus-Vital", price: "$50", modalContent: "...", headerColor: "#ed4a26" },
-  { id: 5, title: "Presto-Plus", text: "Presto-Plus", price: "$50", modalContent: "...", headerColor: "#d72023" },
-  { id: 6, title: "Brokel-K", text: "Brokel-K", price: "$50", modalContent: "...", headerColor: "#2a76b6" },
-  { id: 7, title: "FORTASIL", text: "FORTASIL", price: "$50", modalContent: "...", headerColor: "#abadae" },
-  { id: 8, title: "Impulso-Floral", text: "Impulso-Floral", price: "$50", modalContent: "...", headerColor: "#f0569b" },
-  { id: 9, title: "Calibre-Máximo", text: "Calibre-Máximo", price: "$50", modalContent: "...", headerColor: "#fece38" },
-  { id: 10, title: "Llenado-Brix", text: "Llenado-Brix", price: "$50", modalContent: "...", headerColor: "#75c9ba" },
-  { id: 11, title: "Meta-Ideal", text: "Meta-Ideal", price: "$50", modalContent: "...", headerColor: "#888857" },
-  { id: 12, title: "BOOSTER ROOT", text: "BOOSTER ROOT", price: "$50", modalContent: "...", headerColor: "#533318" },
-  { id: 13, title: "Control-BF", text: "Control-BF", price: "$50", modalContent: "...", headerColor: "#734fa0" },
-  { id: 14, title: "AERO-POLEN", text: "AERO-POLEN", price: "$50", modalContent: "...", headerColor: "#fcb731" },
+  { id: 1, title: "Alfa-Desarrollo", text: "Alfa-Desarrollo", price: "$50", modalContent: "...", headerColor: "#5eb853",pdfFileName:"alfa-desarrollo.pdf"},
+  { id: 2, title: "Brote-Fuerte", text: "Brote-Fuerte", price: "$50", modalContent: "...", headerColor: "#8c5b30",pdfFileName:"brote-fuerte.pdf"},
+  { id: 3, title: "Verde-Forte", text: "Verde-Forte", price: "$50", modalContent: "...", headerColor: "#74c272",pdfFileName:"verde-forte.pdf"},
+  { id: 4, title: "Optimus-Vital", text: "Optimus-Vital", price: "$50", modalContent: "...", headerColor: "#ed4a26",pdfFileName:"optimus-vital"},
+  { id: 5, title: "Presto-Plus", text: "Presto-Plus", price: "$50", modalContent: "...", headerColor: "#d72023",pdfFileName:"presto-plus"},
+  { id: 6, title: "Brokel-K", text: "Brokel-K", price: "$50", modalContent: "...", headerColor: "#2a76b6",pdfFileName:"brokel-k.pdf"},
+  { id: 7, title: "FORTASIL", text: "FORTASIL", price: "$50", modalContent: "...", headerColor: "#abadae",pdfFileName:"fortasil.pdf"},
+  { id: 8, title: "Impulso-Floral", text: "Impulso-Floral", price: "$50", modalContent: "...", headerColor: "#f0569b",pdfFileName:"impulso-floral.pdf"},
+  { id: 9, title: "Calibre-Máximo", text: "Calibre-Máximo", price: "$50", modalContent: "...", headerColor: "#fece38",pdfFileName:"calibre-maximo.pdf"},
+  { id: 10, title: "Llenado-Brix", text: "Llenado-Brix", price: "$50", modalContent: "...", headerColor: "#75c9ba",pdfFileName:"llenado-brix.pdf"},
+  { id: 11, title: "Meta-Ideal", text: "Meta-Ideal", price: "$50", modalContent: "...", headerColor: "#888857",pdfFileName:"meta-ideal.pdf"},
+  { id: 12, title: "BOOSTER-ROOT", text: "BOOSTER-ROOT", price: "$50", modalContent: "...", headerColor: "#533318",pdfFileName:"booster-root.pdf"},
+  { id: 13, title: "Control-BF", text: "Control-BF", price: "$50", modalContent: "...", headerColor: "#734fa0",pdfFileName:"control-bf.pdf"},
+  { id: 14, title: "AERO-POLEN", text: "AERO-POLEN", price: "$50", modalContent: "...", headerColor: "#fcb731",pdfFileName:"aero-polen.pdf"},
 ];
 
 
@@ -81,33 +81,51 @@ const cards = [
             onClick={(e) => e.stopPropagation()}
           >
             {/* Header dinámico */}
-            <div
-              className="flex justify-between items-center p-4 rounded-tl-4xl rounded-tr-4xl"
-              style={{ backgroundColor: cards.find(c => c.id === openModal).headerColor }}
-            >
-              <h2 className="text-white font-bold text-lg">
-                {cards.find(c => c.id === openModal).title}
-              </h2>
-              <div className="flex items-center gap-4">
-                <button
-                  className="text-white hover:opacity-80"
-                  aria-label="Descargar ficha técnica"
-                  onClick={() => alert("Descargar ficha técnica")}
-                >
-                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="48" viewBox="0 0 54 68" fill="none">
-  <path d="M33.75 0.255676L54 20.4174V60.741C54 62.5234 53.2888 64.2328 52.023 65.4931C50.7571 66.7535 49.0402 67.4616 47.25 67.4616H6.75C4.95979 67.4616 3.2429 66.7535 1.97703 65.4931C0.711158 64.2328 0 62.5234 0 60.741V6.97626C0 5.19385 0.711158 3.48445 1.97703 2.22409C3.2429 0.963736 4.95979 0.255676 6.75 0.255676H33.75ZM47.25 60.741V23.7777H30.375V6.97626H6.75V60.741H47.25ZM27 57.3807L13.5 43.9395H21.9375V33.8586H32.0625V43.9395H40.5L27 57.3807Z" fill="white"/>
-</svg>
-                </button>
-                <span className="text-white font-semibold text-3xl">Ficha Técnica</span>
-                <button
-                  className="text-white text-xl font-bold"
-                  onClick={() => setOpenModal(null)}
-                  aria-label="Cerrar modal"
-                >
-                  &times;
-                </button>
-              </div>
-            </div>
+         <div
+  className="flex justify-between items-center p-4 rounded-tl-4xl rounded-tr-4xl"
+  style={{ backgroundColor: cards.find(c => c.id === openModal).headerColor }}
+>
+  <h2 className="text-white font-bold text-base sm:text-lg md:text-xl">
+    {cards.find(c => c.id === openModal).title}
+  </h2>
+
+  <div className="flex items-center gap-2 sm:gap-4">
+    {/* Icono SVG responsivo */}
+   <a
+  href={`/pdfs/${cards.find(c => c.id === openModal).pdfFileName}`}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="text-white hover:opacity-80"
+  aria-label="Abrir ficha técnica"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10"
+    viewBox="0 0 54 68"
+    fill="none"
+  >
+    <path
+      d="M33.75 0.255676L54 20.4174V60.741C54 62.5234 53.2888 64.2328 52.023 65.4931C50.7571 66.7535 49.0402 67.4616 47.25 67.4616H6.75C4.95979 67.4616 3.2429 66.7535 1.97703 65.4931C0.711158 64.2328 0 62.5234 0 60.741V6.97626C0 5.19385 0.711158 3.48445 1.97703 2.22409C3.2429 0.963736 4.95979 0.255676 6.75 0.255676H33.75ZM47.25 60.741V23.7777H30.375V6.97626H6.75V60.741H47.25ZM27 57.3807L13.5 43.9395H21.9375V33.8586H32.0625V43.9395H40.5L27 57.3807Z"
+      fill="white"
+    />
+  </svg>
+</a>
+
+    {/* Texto responsivo */}
+    <span className="text-white font-semibold text-lg sm:text-xl md:text-2xl">
+      Ficha Técnica
+    </span>
+
+    <button
+      className="text-white text-lg sm:text-xl font-bold"
+      onClick={() => setOpenModal(null)}
+      aria-label="Cerrar modal"
+    >
+      &times;
+    </button>
+  </div>
+</div>
+
 
             {/* Contenido del modal */}
             <div className="p-8">
